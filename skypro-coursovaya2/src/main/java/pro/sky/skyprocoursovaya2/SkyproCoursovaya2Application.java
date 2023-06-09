0d191caa8dd7514637a6bce9f2a34426fc392d3b
Александра <bzzz18@yandex.ru>
2023-06-09 15:15:55 +0300

    hello

---
+++ skypro-coursovaya2/src/main/java/pro/sky/skyprocoursovaya2/SkyproCoursovaya2Application.java
@@ -0,0 +1,13 @@
+package pro.sky.skyprocoursovaya2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproCoursovaya2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyproCoursovaya2Application.class, args);
+	}
+
+}
